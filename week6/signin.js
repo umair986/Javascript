988225a0813ac1ab5be62ec29e8e2a2a64876e9e
Shaikh Mohammed Umair <mohumair1901@gmail.com>
2024-11-08 20:52:03 +0530

    basic jwt done

--- week6/signin.js
+++ week6/signin.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "USER_APP";
 
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
+
 
 
 app.post("/signup", function(req, res){
@@ -54,7 +57,10 @@ app.post("/signin", function(req, res){
     const user = users.find(user => user.username === username && user.password === password);
 
     if (user){
-        const token = generateToken();
+        const token = jwt.sign({
+            username: user.username
+        },JWT_SECRET); //convert their username over to a token
+
         user.token = token;
         res.send({
          token
@@ -72,10 +78,14 @@ app.post("/signin", function(req, res){
 
 app.get("/me", function(req, res){
     const token = req.headers.token;
+    const userDetails = jwt.verify(token, JWT_SECRET);
+    const username = userDetails.username;
+
     const user = users.find(user => user.token === token);
     if(user){
         res.send({
-            username : user.username
+            username : user.username,
+            password : user.password
         })
    } else{
         res.status(401).send({
