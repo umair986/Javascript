0293f2b38acf07c7b42b2bca99bb0be3d8d2b4c2
Shaikh Mohammed Umair <mohumair1901@gmail.com>
2024-12-08 18:43:22 +0530

    hash pass 1

--- week7/index.js
+++ week7/index.js
@@ -1,40 +1,27 @@
 // https://petal-estimate-4e9.notion.site/Databases-and-MongoDb-1017dfd107358065a996cda5ed89682e
-
+const bcrypt = require("bcrypt");
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = "123123";
 const mongoose = require("mongoose");
+const {auth , JWT_SECRET} = require('./auth');
 const { UserModel, TodoModel } = require('./db');
 mongoose.connect("mongodb+srv://mohumair1901:mohumair1901@cluster0.r4h0l.mongodb.net/todo-app-database");
 
 const app = express();
 app.use(express.json());
 
-function auth (req, res , next){
-    const token = req.headers.token;
-    const decodedData = jwt.verify(token , JWT_SECRET);
 
-    if(decodedData){
-        req.userID = decodedData.id;
-        next()
-    }
-    else{
-        res.status(403).json({
-            message : "Auth denied"
-        })
-    }
-
-
-};
 
 app.post("/signup", async function(req, res) {
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
+    const hashedPassword = await bcrypt.hash(password, 5);
+    console.log(hashedPassword);
+
      await UserModel.create({
         email: email,
-        password: password,
+        password: hashedPassword,
         name: name
     });
     
