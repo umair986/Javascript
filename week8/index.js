c702fd898f90cbab709935198ad6f6bb1fb19678
Shaikh Mohammed Umair <mohumair1901@gmail.com>
2025-01-17 14:48:32 +0530

    added git ignore

--- week8/index.js
+++ week8/index.js
@@ -5,23 +5,23 @@ const {auth , JWT_SECRET} = require('./auth');
 const app = express();
 app.use(express.json());
 
-app.post("login", function(req,res){
+app.post("/users/signin", function(req,res){
 
 });
 
-app.post("signup", function(req,res){
+app.post("/users/signup", function(req,res){
 
 });
 
-app.post("buy", function(req,res){
+app.post("/course/purchase", function(req,res){
 
 });
 
-app.get("purchased", function(req,res){
+app.get("/user/purchased", function(req,res){
 
 });
 
-app.get("all-courses", function(req,res){
+app.get("/courses", function(req,res){
 
 });
 
